066007226c2b40c7f1d01d4d36d09f6a59cbb117
Erik Pearson <eapearson@lbl.gov>
2016-12-20 13:51:43 -0800

    genericClient no longer requires version if missing, null, or 'auto' it is sent as null, which causes the service wizard to use the default strategy.

--- dist/amd/kb_sdk_clients/genericClient.js
+++ dist/amd/kb_sdk_clients/genericClient.js
@@ -2,7 +2,7 @@
 /*jslint white:true,browser:true,jsnomen:true*/
 define([
     './jsonRpc-native'
-], function (jsonRpc) {
+], function(jsonRpc) {
     'use strict';
 
     /*
@@ -19,12 +19,23 @@ define([
         // Establish an auth object which has properties token and user_id.
         var module = arg.module;
         var token = arg.token || (arg.auth ? arg.auth.token : null);
-        
+
         if (!arg.url) {
             throw new Error('The service discovery url was not provided');
         }
+
+        // The version defaults to null, which signals the service wizard to use the
+        // defaulting protocol, which is using the most "released" version first, 
+        // so release if available, if not beta if available, if not dev, otherwise error.
+        // Note that in a specific environment, the defaulting behavior may be different.
+        // Also, 'auto' implies the same defaulting behavior, but is more explicit.
+        var version;
         if (!arg.version) {
-            throw new Error('The service version was not provided');
+            version = null;
+        } else if (arg.version === 'auto') {
+            version = null;
+        } else {
+            version = arg.version;
         }
 
         function options() {
@@ -35,21 +46,21 @@ define([
             };
         }
 
-        this.lookupModule = function () {
+        this.lookupModule = function() {
             var func = 'get_service_status',
                 params = [{
-                        module_name: module,
-                        version: arg.version || 'dev'
-                    }];
+                    module_name: module,
+                    version: version
+                }];
             // NB: pass null for numRets (number of return values) so we get the 
             // full return structure.
             return jsonRpc.request(arg.url, 'ServiceWizard', func, params, null, options());
         };
-        
+
         this.callFunc = function(funcName, params) {
             // var params = Array.prototype.slice.call(arguments);
             return this.lookupModule()
-                .spread(function (serviceStatus) {
+                .spread(function(serviceStatus) {
                     return jsonRpc.request(serviceStatus.url, module, funcName, params, null, options());
                 });
         };
